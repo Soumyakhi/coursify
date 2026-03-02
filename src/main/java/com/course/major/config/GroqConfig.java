b4b7a18993e47dbc4c95b86be9fb6bb04c2b93bc
Soumyakhi <soumyajitwwe@gmail.com>
2026-03-03 01:31:11 +0530

    implemented pagination

--- src/main/java/com/course/major/config/GroqConfig.java
+++ src/main/java/com/course/major/config/GroqConfig.java
@@ -8,7 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class GrokConfig {
+public class GroqConfig {
 
     @Value("${groq.api.key}")
     private String apiKey;
